31cb83c94f5cda7fee9a0cad2755ab4e4e8bbf65
Julian Poyourow <julianpoyo@gmail.com>
2015-07-14 19:06:54 -0700

    Replaced project root with Angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name linkDumpApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the linkDumpApp
+ */
+angular.module('linkDumpApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name linkDumpApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the linkDumpApp
+ */
+angular.module('linkDumpApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
