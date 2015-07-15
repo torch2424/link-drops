36a09075cf918c746b4c305408d21251df57a5c5
Aaron <torch2424@hotmail.com>
2015-07-14 19:32:56 -0700

    Added some requirements to our mongo project

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require( 'mongoose' );
+var links = mongoose.model( 'link' );
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
