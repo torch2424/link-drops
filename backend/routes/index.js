5a16b3ef1aa529e9ae59497efde29c492e81d047
Aaron <torch2424@hotmail.com>
2015-07-14 19:57:53 -0700

    Made create and read with Julians help for links

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require( 'mongoose' );
-var links = mongoose.model( 'link' );
+var link = mongoose.model( 'link' );
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- backend/routes/links.js
+++ backend/routes/links.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require( 'mongoose' );
+var link = mongoose.model( 'link' );
 
-/* GET users listing. */
+//WE WANT CREATE READ UPDATE DELETE OUR LINKS (CRUD)
+
+/* READ */
+//This will search our links for a link the the backend
+//.find is an ORM feature that will do this for us
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  link.find(function ( err, links, count ){
+      res.json(links);
+  });
+});
+
+/* CREATE. */
+//Post request sending a form to the backend
+router.post('/', function(req, res, next) {
+    //When we submit a form create a new link
+  new link({
+      //json object the a link object contains
+      user_id    : req.body.user_id,
+      content    : req.body.content,
+      updated_at : Date.now()
+  }).save(function( err, todo, count ){
+     //.save will save our new link object in the backend
+    res.json({msg: 'successssssss'});
+  });
 });
 
 module.exports = router;
