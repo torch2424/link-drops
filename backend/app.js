1794850ceb2d5f6a7bca73973300f670a179bb0d
Julian Poyourow <julianpoyo@gmail.com>
2015-07-14 19:30:19 -0700

    Added lots of stuff!

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require( './db' );
+require( './models/db' );
 
 var express = require('express');
 var path = require('path');
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var links = require('./routes/links');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/links', links);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
