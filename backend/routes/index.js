aa95ec5d2a718871dd7d7aaed7fe9b3f44138473
Julian Poyourow <julianpoyo@gmail.com>
2015-07-19 20:03:19 -0700

    Added LOTS of auth stuff

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require( 'mongoose' );
-var link = mongoose.model( 'link' );
+var Dump = mongoose.model( 'Dump' );
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
