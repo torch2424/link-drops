0ad702ebaef3eaf25b3eb76a1d4a6afe361e863b
Aaron <torch2424@hotmail.com>
2015-07-27 16:44:28 -0700

    Started enter link page

--- app/scripts/controllers/links.js
+++ app/scripts/controllers/links.js
@@ -8,10 +8,16 @@
  * Controller of the linkDumpApp
  */
 angular.module('linkDumpApp')
-  .controller('LinksCtrl', function () {
+  .controller('LinksCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
+    ];
+
+    $scope.dumps = [
+        "https://github.com/expressjs/cors",
+        "http://localhost:9000/#/links",
+        "https://keep.google.com/"
     ];
   });
