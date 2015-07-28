b0594396aea0ec9ff36c826b80110c62d28fbdd8
Aaron <torch2424@hotmail.com>
2015-07-27 17:20:36 -0700

    Started getting into the functionalit of the links page, and am starting to allow submitting new links

--- app/scripts/controllers/links.js
+++ app/scripts/controllers/links.js
@@ -8,7 +8,7 @@
  * Controller of the linkDumpApp
  */
 angular.module('linkDumpApp')
-  .controller('LinksCtrl', function ($scope, $sce, $cookies) {
+  .controller('LinksCtrl', function ($scope, $sce, $cookies, Dumps) {
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
@@ -19,18 +19,37 @@ angular.module('linkDumpApp')
     //Get our sessions token
     var sessionToken = $cookies.get("sessionToken");
 
+    //inititalizes our dumps
+    $scope.dumps;
+
     //get our dumps
-    $scope.dumps = [
-        "https://github.com/expressjs/cors",
-        "http://localhost:9000/#/links",
-        "https://keep.google.com/",
-        "https://www.youtube.com/watch?v=t8gu1rh3o-s"
-    ];
+    $scope.getDumps = function()
+    {
+        var dumpResponse = Dumps.get(sessionToken, function()
+        {
+            if(dumpResponse.errorid)
+            {
+
+            }
+            else {
+                //Set scope.dumps to our dumps
+                $scope.dumps = dumpResponse;
+            }
+        });
+    }
 
     //Get a sce trusted iframe youtube link
     $scope.getYoutubeFrame = function(theLink)
     {
         //Get the link on the 33 substring, and trust it
         return $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + theLink.substring(32));
+    }
+
+    //Submit a dumped link
+    $scope.submitLink = function ()
+    {
+        //Our json we will submit to the backend
+        
+        Dumps.save("")
     }
   });
