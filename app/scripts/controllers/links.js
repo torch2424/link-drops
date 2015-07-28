3476ab9741c86574d9e3a896b0178573774c0634
Aaron <torch2424@hotmail.com>
2015-07-27 17:09:14 -0700

    Added dynamic youtube support

--- app/scripts/controllers/links.js
+++ app/scripts/controllers/links.js
@@ -8,7 +8,7 @@
  * Controller of the linkDumpApp
  */
 angular.module('linkDumpApp')
-  .controller('LinksCtrl', function ($scope) {
+  .controller('LinksCtrl', function ($scope, $sce) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -18,6 +18,14 @@ angular.module('linkDumpApp')
     $scope.dumps = [
         "https://github.com/expressjs/cors",
         "http://localhost:9000/#/links",
-        "https://keep.google.com/"
+        "https://keep.google.com/",
+        "https://www.youtube.com/watch?v=t8gu1rh3o-s"
     ];
+
+    //Get a sce trusted iframe youtube link
+    $scope.getYoutubeFrame = function(theLink)
+    {
+        //Get the link on the 33 substring, and trust it
+        return $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + theLink.substring(32));
+    }
   });
