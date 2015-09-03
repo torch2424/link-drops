96ac6790137d4b9fc60d7cdb0ea0246af630ab58
Aaron <torch2424@hotmail.com>
2015-09-03 09:17:31 -0700

    Added a filter to an ngshow on the dump html and it runs amazing and perfect now! :D

--- app/scripts/controllers/links.js
+++ app/scripts/controllers/links.js
@@ -8,7 +8,7 @@
  * Controller of the linkDumpApp
  */
 angular.module('linkDumpApp')
-  .controller('LinksCtrl', function ($scope, $sce, $cookies, $timeout, Dumps, $location, $http, $filter) {
+  .controller('LinksCtrl', function ($scope, $sce, $cookies, $timeout, Dumps, $location, $http) {
 
     this.awesomeThings = [
       'HTML5 Boilerplate',
@@ -50,17 +50,8 @@ angular.module('linkDumpApp')
             else {
                 //Get only the string
                 $scope.dumps = dumpResponse;
-
-                //Get our dumps without a filter
-                $scope.filteredDumps = $filter('filter')($scope.dumps, $scope.enteredFind);
             }
         });
-    }
-
-    //get our filtered dumps
-    $scope.getFiltered = function()
-    {
-        $scope.filteredDumps = $filter('filter')($scope.dumps, $scope.enteredFind);
     }
 
     //Get the title of a link (Using embedly)
