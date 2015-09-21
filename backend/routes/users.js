8129085f03ec2019c7b3750dd40827f87976f719
Julian Poyourow <julianpoyo@gmail.com>
2015-09-21 15:22:30 -0700

    Updated users route to accept email addresses
    Validates and responds correctly.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -19,7 +19,7 @@ router.post('/login', function(req, res, next) {
         });
       } else if (!user) {
         res.status(401).json({
-          msg: "Username does not exist!"
+          msg: "Wrong email!"
         });
       } else {
         //Hash the requested password and salt
@@ -56,55 +56,62 @@ router.post('/login', function(req, res, next) {
 
 /* Join as a user */
 router.post('/join', function(req, res, next) {
-  //Check if a user with that username already exists
-  User.findOne({
-      username: req.body.username.toLowerCase()
-    })
-    .select('_id')
-    .exec(function(err, user) {
-      if (user) {
-        res.status(406).json({
-          msg: "Username already exists!"
-        });
-      } else {
-        //Create a random salt
-        var salt = crypto.randomBytes(128).toString('base64');
-        //Create a unique hash from the provided password and salt
-        var hash = crypto.pbkdf2Sync(req.body.password, salt, 10000, 512);
-        //Create a new user with the assembled information
-        var newUser = new User({
-          username: req.body.username.toLowerCase(),
-          password: hash,
-          salt: salt
-        }).save(function(err, newUser) {
-          if (err) {
-            console.log("Error saving user to DB!");
-            res.status(500).json({
-              msg: "Error saving user to DB!"
-            });
-          } else {
-            //Create a random token
-            var token = crypto.randomBytes(48).toString('hex');
-            //New session!
-            new Session({
-              user_id: newUser._id,
-              token: token
-            }).save(function(err) {
-              if (err) {
-                res.status(500).json({
-                  msg: "Error saving token to DB!"
-                });
-              } else {
-                //All good, give the user their token
-                res.status(201).json({
-                  token: token
-                });
-              }
-            });
-          }
-        });
-      }
+  var emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b/;
+  if (!emailRegex.test(data.username)) {
+    res.status(412).json({
+      msg: "Email is not valid!"
     });
+  } else {
+    //Check if a user with that username already exists
+    User.findOne({
+        username: req.body.username.toLowerCase()
+      })
+      .select('_id')
+      .exec(function(err, user) {
+        if (user) {
+          res.status(406).json({
+            msg: "Email taken!"
+          });
+        } else {
+          //Create a random salt
+          var salt = crypto.randomBytes(128).toString('base64');
+          //Create a unique hash from the provided password and salt
+          var hash = crypto.pbkdf2Sync(req.body.password, salt, 10000, 512);
+          //Create a new user with the assembled information
+          var newUser = new User({
+            username: req.body.username.toLowerCase(),
+            password: hash,
+            salt: salt
+          }).save(function(err, newUser) {
+            if (err) {
+              console.log("Error saving user to DB!");
+              res.status(500).json({
+                msg: "Error saving user to DB!"
+              });
+            } else {
+              //Create a random token
+              var token = crypto.randomBytes(48).toString('hex');
+              //New session!
+              new Session({
+                user_id: newUser._id,
+                token: token
+              }).save(function(err) {
+                if (err) {
+                  res.status(500).json({
+                    msg: "Error saving token to DB!"
+                  });
+                } else {
+                  //All good, give the user their token
+                  res.status(201).json({
+                    token: token
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+  }
 });
 
 
