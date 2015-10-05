9450dfa3657f194df756cab6661d49e348b0d57a
Julian Poyourow <julianpoyo@gmail.com>
2015-10-05 11:55:38 -0700

    Switched envConfig name

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
       options: {
         space: '  ',
         wrap: '"use strict";\n\n {%= __ngModule %}',
-        name: 'config',
+        name: 'envConfig',
       },
       // Development mode environment targets
       development: {
