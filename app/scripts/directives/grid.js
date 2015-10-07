f6dcfc213147b25c7fedc04d8edc1974070b3027
Aaron <torch2424@hotmail.com>
2015-10-07 00:22:19 -0700

    Got the cards displaying and the grid working, simply need some css to get everything going, and check that all functionality is still in place!

--- app/scripts/directives/grid.js
+++ app/scripts/directives/grid.js
@@ -12,12 +12,121 @@ angular.module('linkDumpApp')
       return {
           restrict: 'E',
           replace: true,
-          transclude: true,
           //Need to create our own html file here
           //And copy pasta logic from old controller
-          template: '<div resize ng-style=\'computePositions()\'><div class = "card-panel" ng-repeat=\'dump in list\' ng-transclude>loutre</div></div>',
+          templateUrl: '../views/linkcard.html',
           scope: { list: '=' },
-          controller: function ($scope, $element, $attrs) {
+          controller: function ($scope, $element, $attrs, $http, $sce) {
+
+              //Code for retrieving info of the dump
+
+              //Get the title of a link
+              $scope.getTitle = function(dump, index) {
+
+                //Get the response from noembed
+                $http.get("http://dev.kondeo.com/api/title-scraper.php?q=" + dump.content)
+                  .then(function(response) {
+
+                    //Get the document
+                    var element = document.getElementById("linkTitle-" + index);
+
+                    element.innerHTML = response.data.title;
+                  });
+              }
+
+              //Get a sce trusted noembed
+              $scope.getEmbed = function(dump) {
+                //Get the response from noembed
+                $http.get("https://noembed.com/embed?url=" + dump.content + "&nowrap=on")
+                  .then(function(response) {
+
+                    //Check for no error
+                    if (!response.data.error) {
+                      //Get the document
+                      var element = document.getElementById("embed-" + dump.content);
+
+                      element.innerHTML = $sce.trustAsHtml(response.data.html);
+                      // say the dump has been lazy loaded
+                      dump.lazyEmbed = true;
+                    }
+                  });
+              }
+
+              //Embed an image link
+              $scope.getImage = function(dump) {
+
+                  //pass through the function
+                  elemUrl("img", dump);
+              }
+
+              //Embed a Kickstarter thing
+              $scope.getKickStarter = function(dump) {
+
+                  //Get the embed url
+                  var kickUrl = dump.content.split("?")[0] + "/widget/card.html?v=2";
+
+                  //pass through the function
+                  elemUrl("kick", dump, kickUrl);
+              }
+
+              //Embed a vine thing
+              $scope.getVine = function(dump) {
+
+                  //Get the embed url
+                  var vineUrl = dump.content+ "/embed/simple";
+
+                  //pass through the function
+                  elemUrl("vine", dump, vineUrl);
+              }
+
+              //Embed a spotify (artist, albulm, track) thing
+              $scope.getSpotify = function(dump) {
+
+                  //Get the spotify link type and id , last 2 out of 5 elemnts
+                  var splitUrl = dump.content.split("/");
+
+                  //pass through the function
+                  elemUrl("spotify", dump,
+                  "https://embed.spotify.com/?uri=spotify:"
+                  + splitUrl[3] + ":" +splitUrl[4].split("?")[0]);
+              }
+
+              //get a sce trusted soundcloud thingy
+              $scope.getSoundCloud = function(dump) {
+                //Used this
+                //https://developers.soundcloud.com/docs/api/guide#playing
+
+                SC.get('/resolve', {
+                  url: dump.content
+                }, function(track) {
+
+                    //pass through the function
+                    elemUrl("scWidget", dump,
+                "https://w.soundcloud.com/player/?url=https%3A" +
+                  track.uri.substring(track.uri.indexOf("//api.soundcloud.com")) +
+                  "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true");
+
+                });
+              }
+
+              function elemUrl(id, dump, url) {
+                  //Get the document
+                  var element = document.getElementById( id + "-" + dump.content);
+
+                  if(!url) {
+                      url = dump.content;
+                  }
+
+                  //Set the element src
+                  element.src = $sce.trustAsResourceUrl(url);
+
+                  // say the dump has been lazy loaded
+                  dump.lazyEmbed = true;
+              }
+
+
+              //Tiling code from the wonderful lor Anthony Estebe. Thank you so much
+              //http://microblog.anthonyestebe.com/2013-12-14/grid-pinterest-like-with-angular/
               var _margin, _width, elemHeight, elemPerLine, elemWidth, gridWidth, margin, parent, width;
               parent = $element.parent()[0];
               _margin = null;
