3da6a633475861105433f510b6f8d42a792163f0
Aaron <torch2424@hotmail.com>
2015-10-09 22:59:34 -0700

    Removed Materialize and JQuery

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,7 +23,6 @@ module.exports = function(config) {
     files: [
       // bower:js
       'bower_components/jquery/dist/jquery.js',
-      'bower_components/Materialize/bin/materialize.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
