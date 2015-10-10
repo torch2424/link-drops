07fbaecc2ae27641b020baa64de5d74ba327af68
Aaron <torch2424@hotmail.com>
2015-10-09 23:02:26 -0700

    Added Googles material design library

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,6 +33,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+      'bower_components/material-design-lite/material.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
