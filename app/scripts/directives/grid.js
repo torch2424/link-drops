3c8b61f67826ffb01167c2edc04a7d201f0c9cee
Julian Poyourow <julianpoyo@gmail.com>
2016-02-03 13:00:39 -0800

    WIP performance upgrades

--- app/scripts/directives/grid.js
+++ app/scripts/directives/grid.js
@@ -58,52 +58,6 @@ angular.module('linkDumpApp')
                 gridWidth = function() {
                   return elemPerLine() * elemWidth();
                 };
-
-
-                //Compute positions of elements
-                $scope.computePositions = function() {
-                    var bottom, bottoms, elem, height, i, index, left, maxHeight, top, _i, _j, _len, _len1, _ref;
-                    bottoms = (function() {
-                      var _i, _ref, _results;
-                      _results = [];
-                      for (i = _i = 0, _ref = elemPerLine(); 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
-                        _results.push(0);
-                      }
-                      return _results;
-                    })();
-                    maxHeight = 0;
-                    _ref = $element.children();
-                    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                      elem = _ref[_i];
-                      top = null;
-                      index = 0;
-                      for (i = _j = 0, _len1 = bottoms.length; _j < _len1; i = ++_j) {
-                        bottom = bottoms[i];
-                        if (!((top == null) || top > bottom)) {
-                          continue;
-                        }
-                        top = bottom;
-                        index = i;
-                      }
-                      left = (index * elemWidth()) % gridWidth() + margin();
-                      //Check if our width is less than max width, if it is, have no left
-                      if(width() < maxWidth)
-                      {
-                          left = 0;
-                      }
-                      height = top + elemHeight(elem.offsetHeight);
-                      if (maxHeight < height) {
-                        maxHeight = height;
-                      }
-                      bottoms[index] = height;
-                      elem.style.left = left + 'px';
-                      elem.style.top = top + margin() + 'px';
-                    }
-                    return {
-                      width: "" + (gridWidth()) + "px",
-                      height: "" + maxHeight + "px"
-                    };
-              };
           }
-      };
+      }
   });
